40673757d502f668542dd0e74d87bfbf39f64576
Yohan <yplaques@gmail.com>
2025-01-12 19:58:09 -0300

    Atualizando comando de inicialização e removendo main.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Olá, mundo!"
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json  # Dados enviados pelo Mercado Pago
+    print(data)  # Apenas para testar e ver o conteúdo
+
+    # Aqui você pode adicionar a lógica para processar a notificação, por exemplo:
+    # Verificar se o pagamento foi aprovado, etc.
+
+    return jsonify({'status': 'ok'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
